10c8dbb7b7ae409762ee3104bf2018ce0abe345a
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-15 07:57:03 +0530

    Update h9.py

--- h9.py
+++ h9.py
@@ -2,5 +2,6 @@ n=int(input())
 l=list(map(int,input().split()))
 for i in range(0,n):
     for j in range(i+1,n):
-        if(int(l[i]+l[j])==0):
+        if(int(l[i]+l[j])==0 or int(l[i]+l[j])==1):
             print(l[i],l[j])
+        break  
